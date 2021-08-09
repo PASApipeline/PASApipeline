07bfa14f27c3b98511dbf1b7a6b74ffad2f3e342
Brian Haas <brianjohnhaas@gmail.com>
2021-08-09 22:47:28 +0000

    added test for mysql using a locally running server outside the image

---
+++ Docker/test_docker.MySQL.use_local_server.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+my $base_dir = "$FindBin::Bin/../";
+
+
+my $version = `cat VERSION.txt`;
+chomp $version;
+
+my $cmd = "docker run --rm -it "
+    . " -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock "
+    . " -v $base_dir:$base_dir pasapipeline/pasapipeline:$version "
+    . " bash -c 'service mysql start && cd $base_dir/sample_data && ./runMe.MySQL.sh' ";
+
+print "CMD: $cmd\n";
+
+my $ret = system($cmd);
+
+exit ($ret);
+
+
