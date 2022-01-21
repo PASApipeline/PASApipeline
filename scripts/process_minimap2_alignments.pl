881a5c54881caefb759d830dd917c97c549fb67d
Brian Haas <brianjohnhaas@gmail.com>
2022-01-21 09:16:15 -0500

    including the genome index when writing the minimap2 bam file

--- scripts/process_minimap2_alignments.pl
+++ scripts/process_minimap2_alignments.pl
@@ -111,7 +111,9 @@ main: {
         $splice_param = "--junc-bed $splice_file";
     }
 
-    my $cmd = "bash -c \'set -o pipefail && minimap2 -ax splice $splice_param --secondary=no -O6,24 -B4 -L -t $CPU -cs -ub -G $max_intron_length ${MINIMAP2_CUSTOM_OPTS}  $mm2_idx $transcripts | samtools view -Sb | samtools sort -o $output && samtools index $output \'";
+    my  $genome_samtools_index = "$genomeName.fai";
+    
+    my $cmd = "bash -c \'set -o pipefail && minimap2 -ax splice $splice_param --secondary=no -O6,24 -B4 -L -t $CPU -cs -ub -G $max_intron_length ${MINIMAP2_CUSTOM_OPTS}  $mm2_idx $transcripts | samtools view -Sbt $genome_samtools_index | samtools sort -o $output && samtools index $output \'";
     &process_cmd($cmd);
     
     
